e3ddb3b56600b20a17ed084828089b19019a7725
Alexander <alexgeorgegeer@gmail.com>
2024-11-12 22:41:09 -0600

    select book component and logic

--- client/src/components/admin/CreateMeeting.jsx
+++ client/src/components/admin/CreateMeeting.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import getBooks from "../../api/getBooks";
+import SelectBook from "./SelectBook";
 
 const CreateMeeting = ({ nextMeet }) => {
   const [bookLoading, setBookLoading] = useState(true);
@@ -16,63 +17,74 @@ const CreateMeeting = ({ nextMeet }) => {
     setBookLoading(false);
   };
 
+  const handleSelectBook = (e) => {
+    e.preventDefault();
+    document.getElementById("select_book").showModal();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(meetData);
+  };
+
   useEffect(() => {
     fetchBooks();
   }, []);
 
   return (
-    <form
-      className="flex flex-col gap-4 items-center"
-      // onSubmit={submitSignup}
-    >
-      <h2>Next Meeting</h2>
-      <label hidden htmlFor="email">
-        Location
-      </label>
-      <input
-        type="text"
-        name="location"
-        value={meetData?.location || ""}
-        placeholder="Location"
-        className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
-        onChange={handleMeetChange}
-      />
-      <label hidden htmlFor="username">
-        Date
-      </label>
-      <input
-        type="date"
-        name="date"
-        value={meetData?.date || ""}
-        placeholder="Date"
-        className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
-        onChange={handleMeetChange}
-      />
-      <label hidden htmlFor="password">
-        Time
-      </label>
-      <input
-        type="time"
-        name="time"
-        value={meetData?.time || ""}
-        placeholder="Select Time"
-        className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
-        onChange={handleMeetChange}
-      />
-      {bookLoading ? (
-        <span className="loading loading-bars loading-md"></span>
-      ) : (
-        <div className="flex flex-col lg:flex-row gap-4 lg:gap-8 w-full">
-          <input
-            type="text"
-            name="book"
-            value={meetData?.book || ""}
-            placeholder="Next Book"
-            className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
-            disabled
-            onChange={handleMeetChange}
-          />
-          {/* <select
+    <>
+      <form
+        className="flex flex-col gap-4 items-center"
+        onSubmit={handleSubmit}
+      >
+        <h2>Next Meeting</h2>
+        <label hidden htmlFor="email">
+          Location
+        </label>
+        <input
+          type="text"
+          name="location"
+          value={meetData?.location || ""}
+          placeholder="Location"
+          className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
+          onChange={handleMeetChange}
+        />
+        <label hidden htmlFor="username">
+          Date
+        </label>
+        <input
+          type="date"
+          name="date"
+          value={meetData?.date || ""}
+          placeholder="Date"
+          className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
+          onChange={handleMeetChange}
+        />
+        <label hidden htmlFor="password">
+          Time
+        </label>
+        <input
+          type="time"
+          name="time"
+          value={meetData?.time || ""}
+          placeholder="Select Time"
+          className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
+          onChange={handleMeetChange}
+        />
+        {bookLoading ? (
+          <span className="loading loading-bars loading-md"></span>
+        ) : (
+          <div className="flex flex-col lg:flex-row gap-4 lg:gap-8 w-full">
+            <input
+              type="text"
+              name="book"
+              value={meetData?.book || ""}
+              placeholder="Next Book"
+              className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
+              disabled
+              onChange={handleMeetChange}
+            />
+            {/* <select
             name="book"
             className="select select-bordered rounded-lg py-3 px-3 border-indigo-600 placeholder-white-500 text-white w-full lg:w-1/2"
           >
@@ -87,20 +99,25 @@ const CreateMeeting = ({ nextMeet }) => {
               </option>
             ))}
           </select> */}
-          <button className="btn btn-outline w-full lg:w-1/2">
-            Choose Book
-          </button>
-        </div>
+            <button
+              className="btn btn-outline w-full lg:w-1/2"
+              onClick={handleSelectBook}
+            >
+              Select Book
+            </button>
+          </div>
 
-        //   className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
-        //   onChange={handleSignupChange}
-      )}
+          //   className="border rounded-lg py-3 px-3 bg-transparent border-indigo-600 placeholder-white-500 text-white w-full"
+          //   onChange={handleSignupChange}
+        )}
 
-      <button className="btn btn-wide btn-ghost" type="submit">
-        Create Meeting
-      </button>
-      {/* <Alert content={alertMsg} /> */}
-    </form>
+        <button className="btn btn-wide btn-ghost" type="submit">
+          Create Meeting
+        </button>
+        {/* <Alert content={alertMsg} /> */}
+      </form>
+      <SelectBook books={books} setMeetData={setMeetData} meetData={meetData} />
+    </>
   );
 };
 export default CreateMeeting;
---
+++ client/src/components/admin/SelectBook.jsx
@@ -0,0 +1,39 @@
+const SelectBook = ({ books, setMeetData, meetData }) => {
+  const handleSelectBook = (title) => setMeetData({ ...meetData, book: title });
+  return (
+    <dialog id="select_book" className="modal">
+      <form method="dialog">
+        <div className="flex flex-col gap-4 p-4 bg-slate-950 h-full w-full justify-center">
+          {books.map((book) => (
+            <button
+              value={book.id}
+              key={book.id}
+              className="flex gap-4 lg:gap-8 justify-between items-center btn-outline px-5 py-2 border rounded-lg"
+              onClick={() => handleSelectBook(book.title)}
+            >
+              <div className="flex flex-col gap-2 lg:gap-4 items-start">
+                <h2 className="text-xl">{book.title}</h2>
+                <h3 className="text-l">{book.author}</h3>
+              </div>
+              <figure>
+                <img
+                  className=""
+                  src={`https://covers.openlibrary.org/b/id/${book.cover}-S.jpg`}
+                  alt={`Cover image of ${book.title}`}
+                />
+              </figure>
+            </button>
+          ))}
+
+          <button
+            method="dialog"
+            className="flex gap-4 lg:gap-8 justify-center items-center btn-outline px-5 py-2 border rounded-lg"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </dialog>
+  );
+};
+export default SelectBook;
