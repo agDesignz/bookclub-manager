125c7b47d6e1539fb3ff2c874791e521b43eb43a
Alexander <alexgeorgegeer@gmail.com>
2025-01-03 23:17:59 -0600

    started new design of dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,29 +3,30 @@ import NextMeeting from "./NextMeeting";
 
 const Dashboard = () => {
   return (
-    <section className="dashboard container grid gap-2 sm:gap-4 grid-cols-1 lg:grid-cols-2">
-      {/* <div className="flex flex-col gap-4 items-stretch"> */}
-      <div className="lg:row-span-3">
+    <section className="dashboard container grid gap-6 grid-cols-1 md:grid-cols-2">
+      <h2 className="text-center text-2xl">Current Book</h2>
+      <div className="md:row-start-2 md:row-span-1 md:col-start-1 md:col-span-1">
         <NextMeeting />
       </div>
-      <Link to="/search">
-        <div className="rounded-xl shadow-md px-4 py-8 bg-teal-800">
-          <h2 className="text-center text-2xl">Book Search</h2>
-        </div>
-      </Link>
+      <div className="flex flex-col gap-4 items-stretch md:row-start-2 md:row-span-1 md:col-start-2 md:col-span-1">
+        <Link to="/search">
+          <div className="rounded-xl shadow-md px-4 py-8 bg-teal-800">
+            <h2 className="text-center text-2xl">Book Search</h2>
+          </div>
+        </Link>
 
-      <Link to="/suggestions">
-        <div className="rounded-xl shadow-md px-4 py-8 bg-amber-700">
-          <h2 className="text-center text-2xl">Book Suggestions</h2>
-        </div>
-      </Link>
+        <Link to="/suggestions">
+          <div className="rounded-xl shadow-md px-4 py-8 bg-amber-700">
+            <h2 className="text-center text-2xl">Book Suggestions</h2>
+          </div>
+        </Link>
 
-      <Link to="/archive">
-        <div className="rounded-xl shadow-md px-4 py-8 bg-violet-900">
-          <h2 className="text-center text-2xl">Archives</h2>
-        </div>
-      </Link>
-      {/* </div> */}
+        <Link to="/archive">
+          <div className="rounded-xl shadow-md px-4 py-8 bg-violet-900">
+            <h2 className="text-center text-2xl">Archives</h2>
+          </div>
+        </Link>
+      </div>
     </section>
   );
 };
--- client/src/components/NextMeeting.jsx
+++ client/src/components/NextMeeting.jsx
@@ -9,13 +9,27 @@ const NextMeeting = () => {
   const { meetingLoading, meeting } = useMeetingContext();
 
   return (
-    <>
+    <div className="flex justify-center items-center h-full p-2 md:p-0">
+      {/* <h2 className="text-center text-2xl">Current Book</h2> */}
       {meetingLoading ? (
         <h2>Still Loading</h2>
       ) : meeting ? (
-        <div className="flex flex-col gap-4 p-4">
+        <div className="flex flex-col items-center md:items-start gap-4 md:gap-6 w-full">
+          {/* <div className="flex gap-4 mb-4 md:mb-6 justify-center"> */}
+          <div className="flex flex-col md:grow">
+            <h2 className="text-3xl">{meeting?.book?.title || "TBA"}</h2>
+            <h4>{meeting?.book?.author}</h4>
+          </div>
+          <figure>
+            <img
+              className="h-full w-auto"
+              src={`https://covers.openlibrary.org/b/id/${meeting?.book?.cover}-M.jpg`}
+              alt={`Cover image of ${meeting?.book?.title}`}
+            />
+          </figure>
+          {/* </div> */}
           <p className="text-center">Next Meeting</p>
-          <div className="flex gap-2 items-stretch">
+          <div className="flex flex-wrap gap-2 items-stretch">
             <div className="rounded-md bg-sky-800 p-2">
               <p>{meeting?.date || "TBA"}</p>
             </div>
@@ -26,36 +40,24 @@ const NextMeeting = () => {
               <p className="text-center">{meeting?.location || "TBA"}</p>
             </div>
           </div>
-          <p className="text-center">Current Book</p>
-          <div className="flex gap-4 justify-center">
-            <div className="flex flex-col grow">
-              <h2 className="text-3xl">{meeting?.book?.title || "TBA"}</h2>
-              <h4>{meeting?.book?.author}</h4>
-            </div>
-            <figure>
-              <img
-                className="h-full"
-                src={`https://covers.openlibrary.org/b/id/${meeting?.book?.cover}-M.jpg`}
-                alt={`Cover image of ${meeting?.book?.title}`}
-              />
-            </figure>
-          </div>
-          {/* <div className="w-full border-b border-blue-50 my-2 mx-auto"></div> */}
-
           {userData.isAdmin && (
-            <>
-              {/* <div className="w-full border-b border-blue-50 my-2 mx-auto"></div> */}
-              <Link className="btn btn-wide btn-outline grow" to="/admin">
-                Update or Create Meeting
-              </Link>
-            </>
+            <Link
+              className="btn btn-wide btn-outline grow h-auto min-h-10"
+              to="/admin"
+            >
+              Update or Create Meeting
+            </Link>
           )}
-          {/* <div className="w-full border-b border-blue-50 my-2 mx-auto"></div> */}
         </div>
       ) : (
-        <div>No Meetings Yet</div>
+        <div className="flex flex-col gap-4 md:gap-8">
+          <h2 className="text-2xl text-center">No Meetings Yet</h2>
+          <Link className="btn btn-wide btn-outline grow" to="/admin">
+            Create Meeting
+          </Link>
+        </div>
       )}
-    </>
+    </div>
   );
 };
 export default NextMeeting;
